a29ebc0a39bb9f4a3343c0d217b598808e67d773
Robert Hintz <rhintz42@stanford.edu>
2012-08-27 21:45:20 -0700

    Taking out garden_cookbook because should have just rails vag installation

--- source/garden_cookbook/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-GardenCookbook::Application.config.secret_token = 'ff008156c96df1a7c0d36be8ee66bc53f0434d048369074d655afd74074f6637fa116673036fac1d50f2a65c74b3675bd43b9d87986715a978892fc14a9aebe9'
