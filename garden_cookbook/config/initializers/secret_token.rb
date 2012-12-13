bfb2b3197a13c70c87188c1106e6e8c5ca17d3cc
roberth <roberth@roberth-ThinkPad-X220>
2012-12-12 19:22:37 -0800

    Changed Gemfile in garden_cookbook

---
+++ garden_cookbook/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GardenCookbook::Application.config.secret_token = '73287c8c8e4f29bac8afcdccc3516aa83d05377d5dd8667304be66cdb91b5a664b2959a0cd7644d3c3dc83ae8382f131885955adc7e6f0091b36ea10508f00f8'
